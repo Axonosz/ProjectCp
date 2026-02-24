d2fb837fe9adb77631b3cc97cb2c7c853c82241c
Axonosz <nattapat.is.me@gmail.com>
2026-02-24 11:50:26 +0700

    FIxEnemyBug.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -4,21 +4,19 @@
 Enemy::Enemy(int x, int y) : Entity(x,y) {}
 
 void Enemy::update(int px, int py, Map& map) {
-    int nx = x;
-    int ny = y;
+    int dx = 0;
+    int dy = 0;
 
-    if (px > x) nx++;
-    else if (px < x) nx--;
+    if (px > x) dx = 1;
+    else if (px < x) dx = -1;
+    else if (py > y) dy = 1;
+    else if (py < y) dy = -1;
+
+    int nx = x + dx;
+    int ny = y + dy;
 
     if (!map.isWall(nx, ny)) {
         x = nx;
-        return;
-    }
-
-    if (py > y) ny++;
-    else if (py < y) ny--;
-
-    if (!map.isWall(nx, ny)) {
         y = ny;
     }
 }
